4adaca166d1746acc6028f9e362ce24b252d304c
Jered Stevens <j.stevens8185@gmail.com>
2022-04-13 08:40:47 -0500

    updates

--- HW_2/CallenderStevensH2.cpp
+++ HW_2/CallenderStevensH2.cpp
@@ -21,35 +21,7 @@ int main()
 {
     int decision = 1;
     Fraction a,b;
-    // long int a = 4, b = 14;
-    // Fraction c;
-    // Fraction d = 5;
-    // Fraction e(2,7);
-    // c.set(a, b);
-    // if(c!=d){
-    //     cout << "C and D are not equal" << endl;
-    // }else{
-    //     cout << "your not equal operator is broken" << endl;
-    // };
-    // if(c!=e){
-    //     cout << "Your not equal operator is broken" << endl;
-    // }else{
-    //     cout << "c and e are equal" << endl;
-    // };
-    // if(e>=c){
-    //     cout << "It's an older code but it checks out" << endl;
-    // }else{
-    //     cout << "less than operator is broken" << endl;
-    // }
-    // cout << "c is " << c << endl;
-    // cout << "reciporical of c is " << c.reciporical() << endl;
-    // cout << "c to the power of 3 is: " << c.exp(3) << endl;
-    // cout << "c as a double is " << double(c) << endl;
-    // c = d;
-    // cout << "c is now equal to d. C is: " << c << endl;
-    // cout << "Enter a fraction" << endl;
-    // cin >> c;
-    // cout << c;
+
     while(decision != 0){
         run(a,b);
         cout << "WOULD YOU LIKE TO START OVER? YES--->(any number other than 0) ";
