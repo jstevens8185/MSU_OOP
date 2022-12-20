7511f3293cff69164d29efd135d012c9709ceb2b
jstevens8185 <j.stevens8185@gmail.com>
2022-12-20 12:58:44 -0600

    Delete redundant files

--- HW_4/main.cpp
+++ HW_4/main.cpp
@@ -8,21 +8,7 @@ using namespace std;
 int AccountNumber::acctCounter = 0;
 
 
-/////////////////////////////////////////////////
-//	STILL NEEDED:
-//		functionality to:
-//		-withdraw
-//		-deposit
-//		-send money to another account
-//		-view balance
-/////////////////////////////////////////////////
-
 int main() {
-	//string UnitedStates = "united states";
-	//string* usa = &UnitedStates;
-	//Student Jered("Jered", "Stevens", "10-20-1998", "3004 Barrywood Drive", usa, "CompSci", "Tina Johnson", 3);
-	//cout << Jered;
-
 	Bank bank;
 	bank.createNewAccount();
 	bank.printStudentAccounts();
